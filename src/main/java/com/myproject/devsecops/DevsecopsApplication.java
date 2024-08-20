2ce3b0ef12a7eaee72ab851cefad86c7d4a4630f
Aman Yadav <57805090+amanyadav722@users.noreply.github.com>
2024-08-20 10:26:48 -0400

    Updated hello world again

--- src/main/java/com/myproject/devsecops/DevsecopsApplication.java
+++ src/main/java/com/myproject/devsecops/DevsecopsApplication.java
@@ -8,7 +8,6 @@ public class DevsecopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevsecopsApplication.class, args);
-		System.out.println("Hello, World!");
 	}
 
 }
